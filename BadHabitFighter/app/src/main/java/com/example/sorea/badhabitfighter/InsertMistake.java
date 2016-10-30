c435be8ce857f6887c72d1ddcce7c71b48418a09
Kenneth Adair <SOREASAN@GMAIL.COM>
2016-10-30 14:25:40 -0600

    Created fragments
    I created all the front-end fragments I think I need.  I created a
    fragment for the login screen, the screen that displays all the
    mistakes, the screen that is used to edit/delete/select an existing
    fragment (the edit one), and a screen to insert mistakes.  I also did
    some ghetto state management by having my program only load fragments if
    the statemanagement thing is null.  I don't know if that's the correct
    or best way to do it but I'm not getting errors when I rotate my screen
    so I'm leaving it for now and I'll come back if I have problems.  I
    still haven't created any Java classes for the back end which I'll need
    to do.  Specifically I'll need some classes to manage the database.

---
+++ BadHabitFighter/app/src/main/java/com/example/sorea/badhabitfighter/InsertMistake.java
@@ -0,0 +1,29 @@
+package com.example.sorea.badhabitfighter;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class InsertMistake extends Fragment {
+
+
+    public InsertMistake() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_insert_mistake, container, false);
+    }
+
+}
--- BadHabitFighter/app/src/main/java/com/example/sorea/badhabitfighter/MainActivity.java
+++ BadHabitFighter/app/src/main/java/com/example/sorea/badhabitfighter/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.sorea.badhabitfighter;
 
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+    //My theory is that I can do state management by having this global fragment.
+    Fragment currentFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //Default stuff
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if(savedInstanceState == null) {
+            currentFragment = new LoginScreen();
+            Log.d("test", "Creating fragments for the first time");
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.activity_main, currentFragment,"mainLayout")
+                    .addToBackStack(null)
+                    .commit();
+            getSupportFragmentManager().executePendingTransactions();
+        }
     }
 }
